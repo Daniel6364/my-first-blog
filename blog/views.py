ffa5f4f2831c2fb4a22ed163ce842e03575b9431
Daniel6364 <iamsung33@naver.com>
2019-01-07 00:04:45 +0900

    added template aerial.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 
 def index (request):
-	return render(request, 'blog/index.jsp')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/index.html', {'posts' : posts})
+
+
+
+
+def aerial (request):
+	
+	return render(request, 'blog/aerial.html')
